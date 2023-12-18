b968a553eb16e774d3e05cc2dc141b4b1d56ddf0
Prashanth1698 <139166001+Prashanth1698@users.noreply.github.com>
2023-12-18 20:58:00 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+//primitive
+
+let firstname = "prashanth"; 
+let age = "25";
+let isYoung = "true";
+let lastname;
+
+console.log(lastname);
+console.log(age);
+console.log(isYoung);
+console.log(firstname);
