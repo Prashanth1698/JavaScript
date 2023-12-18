ece801f8e25815b1753274736df4a330d0eb2fbe
Prashanth1698 <139166001+Prashanth1698@users.noreply.github.com>
2023-12-18 20:46:09 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,21 @@
+let name = "prasii";
+name = "prashanth"
+let msg = "hello i am  "+ name +" learning javascript"
+//alert
+//alert(msg)
+//logging
+console.log(msg)
+
+
+let number1 = 40;
+let number2 = 5
+let area = number1+number2;
+console.log(area)
+
+//constant
+
+const first = "prashanth";
+const last = "Ganesan";
+let full = first+last;
+console.log(full);
+
