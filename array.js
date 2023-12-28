2f98069eeb858907ef3d143eece68d1655e71266
Prashanth1698 <139166001+Prashanth1698@users.noreply.github.com>
2023-12-28 10:45:24 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,7 @@
+let favcolor = ["blue","yellow","pink"]
+
+favcolor[4] = 16 // assigned the value 16 to the element at index 4
+
+console.log(favcolor); // outputs the entire array with the assigned value at index 4.
+console.log(favcolor[2]); // outputs the value at index 2, which is "pink". 
+console.log(favcolor.length); // outputs the length of the array, which is 5.
