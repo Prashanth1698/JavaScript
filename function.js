7672ee1c9b3970548cc360e60aa34e9cbeca003c
Prashanth1698 <139166001+Prashanth1698@users.noreply.github.com>
2023-12-28 11:09:17 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+function greetUser(firstName, lastName){
+
+let msg = " Hello "+firstName+" "+lastName+" I'm learning Javascript";
+
+console.log(msg); //log output  
+
+}
+greetUser("Prashanth", "Ganesan")
+
+function add(number1, number2){
+    console.log(number1 + number2);
+}
+add(20 , 20)
